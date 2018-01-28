fd37fc9ade69f794032aff2489508f4615d5c353
Loic Shyaka <loicshyaka09@gmail.com>
2018-01-28 14:51:12 +0800

    Initial Commit

---
+++ ClothingStoreFilter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClothingStoreFilter
+//
+//  Created by Loic Shyaka on 1/28/18.
+//  Copyright © 2018 721 Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
